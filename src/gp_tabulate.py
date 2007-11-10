4fbf20cf5168f8916d46a9d6657320fc25d81394
rpc25 <rpc25@8e462c0f-ee70-4953-a96e-5db927b55778>
2007-11-10 12:13:29 +0000

    Multiple function tabulation in gp_tabulate
    git-svn-id: svn://127.0.0.1/pplsvn@123 8e462c0f-ee70-4953-a96e-5db927b55778

--- src/gp_tabulate.py
+++ src/gp_tabulate.py
@@ -43,25 +43,35 @@ using_use_warned = False
 # DIRECTIVE_TABULATE(): Handles the 'tabulate' command
 
 def directive_tabulate(command,vars,settings):
-  # First allocate an x-axis and a y-axis, the two axes we have by default
-  axes = { 'x':{}, 'y':{}, 'z':{} }
+  gridlist = []  # List of grids to output from all our functions
+
+  # Global options: currently only the format string
+  # format string
+  if 'format' in command: format = command['format']
+  else                  : format = ''
+   
+  # Faff with axes.  We need separate axes for function and datafile evaluation (unless axes have been specified completely)
+  funcaxes = { 'x':{}, 'y':{}, 'z':{} }
+  fileaxes = { 'x':{}, 'y':{}, 'z':{} }
   for dir in ['x', 'y']:
-   axes[dir][1] = gp_settings.axes[dir][1].copy()
+   funcaxes[dir][1] = gp_settings.axes[dir][1].copy()
+   fileaxes[dir][1] = gp_settings.axes[dir][1].copy()
 
   # If requested, change the ranges of these axes
   for i in range(min(1, len(command['range_list']))):
    if ((i%2) == 0): direction='x'
    else           : direction='y'
    number=int(i/2)+1
-   if 'min' in command['range_list'][i]: axes[direction][number]['MIN'] = command['range_list'][i]['min']
-   if 'max' in command['range_list'][i]: axes[direction][number]['MAX'] = command['range_list'][i]['max']
+   if 'min' in command['range_list'][i]: 
+    fileaxes[direction][number]['MIN'] = command['range_list'][i]['min']
+    funcaxes[direction][number]['MIN'] = command['range_list'][i]['min']
+   if 'max' in command['range_list'][i]: 
+    fileaxes[direction][number]['MAX'] = command['range_list'][i]['max']
+    funcaxes[direction][number]['MAX'] = command['range_list'][i]['max']
 
-  # Step 1: Work out the ranges of the data, to decide how to autoscale.
-  #         This is achieved with a dry-run of the plotting process.
-  any_autoscaling_axes = 0
+  # Step 1: Set up the axes structure for function evaluation
   for direction in 'x', 'y':
-   number = 1
-   axis = axes[direction][number]
+   axis = funcaxes[direction][1]
    axis['MIN_USED'] = axis['MIN']
    axis['MAX_USED'] = axis['MAX']
    axis['AXIS']     = None
@@ -76,75 +86,136 @@ def directive_tabulate(command,vars,settings):
      axis['MAX'] = None
      gp_warning("Warning: Log axis %s%d set with range maximum < 0 -- this is impossible, so autoscaling instead."%(direction,number))
 
-   if ((axis['MIN_USED'] == None) or (axis['MIN_USED'] == None)):
-    any_autoscaling_axes = 1
-
-  # Read in details supplied on the commandline
-
-  # Read datafile filename
-  if 'filename' in command:
-   datafile = command['filename']
-  else:
-   functions = [item['expression'] for item in command['expression_list:'] ]
-
-  # every
-  if 'every_list:' in command: every = command['every_list:']
-  else                       : every = []
-
-  # index
-  if 'index' in command: index = int(command['index'])
-  else                 : index = -1 # default
-
-  # select
-  if 'select_criterion' in command: select_criteria = command['select_criterion']
-  else                            : select_criteria = ''
-  select_cont = True
-  if (('select_cont' in command) and (command['select_cont'][0] == 'd')):
-   select_cont = False
-
-  # Using rows or columns
-  if   'use_rows'    in command: usingrowcol = "row"
-  elif 'use_columns' in command: usingrowcol = "col"
-  else                         : usingrowcol = "col" # default
-
-  # using
-  if 'using_list:' in command: using = [item['using_item'] for item in command['using_list:']]
-  else                       : using = []
-
-  # format string
-  if 'format' in command: format = command['format']
-  else                  : format = ''
-
-  if 'filename' in command:  # We are plotting a datafile...
-   datagrid = gp_datafile.gp_dataread(datafile, index, usingrowcol, using, select_criteria, select_cont, every, vars, "tabulate", True, None)
-  else: # We are plotting a function...
-   if (axes['x'][1]['MIN'] == None): # Automatic range choices
-    if (axes['x'][1]['LOG'] == 'ON'):
-     if (axes['x'][1]['MAX'] != None): axes['x'][1]['MIN'] = axes['x'][1]['MAX'] / 100 # Log axes start from 1
-     else                : axes['x'][1]['MIN'] = 1.0
-    else:
-     if (axes['x'][1]['MAX'] != None): axes['x'][1]['MIN'] = maximum - 20
-     else                : axes['x'][1]['MIN'] = -10.0         # Lin axes start from -10
-   if (axes['x'][1]['MAX'] == None):
-    if (axes['x'][1]['LOG'] == 'ON'): axes['x'][1]['MAX'] = axes['x'][1]['MIN'] * 100
-    else                                             : axes['x'][1]['MAX'] = axes['x'][1]['MIN'] + 20
-
-   # Obtain a raster
-   if (axes['x'][1]['LOG'] == 'ON'): xrast = gp_math.lograst(axes['x'][1]['MIN'], axes['x'][1]['MAX'], settings['SAMPLES'])
-   else:                             xrast = gp_math.linrast(axes['x'][1]['MIN'], axes['x'][1]['MAX'], settings['SAMPLES'])
-   
-   # Obtain the data grid for a function
-   datagrid = gp_datafile.gp_function_datagrid(xrast, functions, 'x', usingrowcol, using, select_criteria, select_cont, every, vars, 'tabulate', True, None)
+    # Automatic range choices
+  if (funcaxes['x'][1]['MIN'] == None): 
+   if (funcaxes['x'][1]['LOG'] == 'ON'):
+    if (funcaxes['x'][1]['MAX'] != None): funcaxes['x'][1]['MIN'] = funcaxes['x'][1]['MAX'] / 100 # Log axes start from 1
+    else                                : funcaxes['x'][1]['MIN'] = 1.0
+   else:
+    if (funcaxes['x'][1]['MAX'] != None): funcaxes['x'][1]['MIN'] = funcaxes['x'][1]['MAX'] - 20
+    else                                : funcaxes['x'][1]['MIN'] = -10.0         # Lin axes start from -10
+  if (funcaxes['x'][1]['MAX'] == None):
+   if (funcaxes['x'][1]['LOG'] == 'ON'): funcaxes['x'][1]['MAX'] = funcaxes['x'][1]['MIN'] * 100
+   else                                : funcaxes['x'][1]['MAX'] = funcaxes['x'][1]['MIN'] + 20
   
-  # Filter the data to remove any datapoints which fall outside the range of the axes
-  datagrid = filter_dataset(datagrid, axes)
-
-  if (len(datagrid) == 1):
-   gp_warning("Warning: No data to tabulate!")
+  # Obtain a raster
+  if (funcaxes['x'][1]['LOG'] == 'ON'): xrast = gp_math.lograst(funcaxes['x'][1]['MIN'], funcaxes['x'][1]['MAX'], settings['SAMPLES'])
+  else:                                 xrast = gp_math.linrast(funcaxes['x'][1]['MIN'], funcaxes['x'][1]['MAX'], settings['SAMPLES'])
+    
+  # Step 2: Assemble our blocks of data to plot
+  print command
+  if not 'tabulate_list,' in command: 
+   gp_warning('Nothing to tabulate!')
    return
 
+  # Iterate through each item to tabulate and obtain the corresponding data grid
+  for tabulate_item in command['tabulate_list,']:
+    
+   # every
+   if 'every_list:' in tabulate_item: every = tabulate_item['every_list:']
+   else                             : every = []
+   
+   # index
+   if 'index' in tabulate_item: index = int(tabulate_item['index'])
+   else                       : index = -1 # default
+   
+   # select
+   if 'select_criterion' in tabulate_item: select_criteria = tabulate_item['select_criterion']
+   else                                  : select_criteria = ''
+   if (('select_cont' in tabulate_item) and (tabulate_item['select_cont'][0] == 'd')): select_cont = False
+   else                                                                              : select_cont = True
+   
+   # Using rows or columns
+   if   'use_rows'    in tabulate_item: usingrowcol = "row"
+   elif 'use_columns' in tabulate_item: usingrowcol = "col"
+   else                               : usingrowcol = "col" # default
+   
+   # using
+   if 'using_list:' in tabulate_item: using = [item['using_item'] for item in tabulate_item['using_list:']]
+   else                             : using = []
+   
+   if 'filename' in tabulate_item:  # We are plotting a datafile...
+    datafile = tabulate_item['filename']
+    type = 'file'
+    datagrid = gp_datafile.gp_dataread(datafile, index, usingrowcol, using, select_criteria, select_cont, every, vars, "tabulate", True, None)
+   else: # We are plotting a function...
+    functions = [item['expression'] for item in tabulate_item['expression_list:'] ]
+    type = 'func'
+    if (using != []): type = 'func_using'
+    # Obtain the data grid for a function
+    datagrid = gp_datafile.gp_function_datagrid(xrast, functions, 'x', usingrowcol, using, select_criteria, select_cont, every, vars, 'tabulate', True, None)
+   
+   # Filter the data to remove any datapoints which fall outside the range of the axes
+   datagrid = filter_dataset(datagrid, fileaxes) # fileaxes is correct here because it's the least restrictive of the two (reflects only user input)
+   
+   if (len(datagrid) == 1):
+    gp_warning("Warning: No data to tabulate!")
+    return
+   
+   gridlist.append([datagrid[1:], type])
+
+  # Step 3: Taking our list of grids of data, make them into one big grid
+
+  # First, deal with the simple case where there is just one grid
+  if (len(gridlist)==1): 
+   output_table(gridlist[0][0], settings, format)
+   return
+
+  # First, functions tabulated without the "using" modifier should have their x raster stripped, other than for the first one
+  # This is so, e.g. "tab sin(x),cos(x)" produces three columns
+  foundfunc = 0
+  for grid in range(len(gridlist)):
+   if gridlist[grid][1] != 'func': continue # Check type == func
+   if foundfunc==0:
+    foundfunc = 1
+    continue
+   for block in range(len(gridlist[grid][0])): # Iterate over blocks
+    gridlist[grid][0][block][1] -= 1  # Subtract one from column count
+    for k in range(len(gridlist[grid][0][block][2])):
+     del gridlist[grid][0][block][2][k][0] # Delete x raster value from each datum
+
+  # Get rid of the labels telling us what sort of thing each grid is
+  for grid in range(len(gridlist)): gridlist[grid] = gridlist[grid][0]
+   
+  # Check that the block structure is the same for each grid of data.  If not, collapse each one into a single block
+  blocks_differ = 0
+  blocks = []
+  for grid in range(len(gridlist)):
+   blocks.append([gridlist[grid][block][0] for block in range(len(gridlist[grid]))])
+  for i in range(1,len(blocks)):
+   if (blocks[i] != blocks[0]):
+    blocks_differ = 1
+    break
+  
+  if (blocks_differ == 1):
+   gp_warning("Blocks don't match; collapsing into a single block")
+   for grid in range(len(gridlist)):
+    for block in range(1,len(gridlist[grid])): 
+     gridlist[grid][0][0] += gridlist[grid][block][0]  # Sum the row numbers
+     gridlist[grid][0][2] += gridlist[grid][block][2] # Catenate the blocks
+     del gridlist[grid][block]                        # Delete the old block
+
+   # Additionally if the block structure was different we may not have blocks the same length.  In this case pad with blanks
+   max_grid_length = gridlist[0][0][0]
+   blocks_differ = 0
+   for grid in range(1,len(gridlist)):
+    if gridlist[grid][0][0] != gridlist[0][0][0]:
+     blocks_differ = 1
+     max_grid_length = max(max_grid_length, gridlist[grid][0][0])
+   # Produce correctly-sized blank points and pad
+   for grid in range(len(gridlist)):
+    point = []
+    for i in range(len(gridlist[grid][0][1])): point.append('')
+    for i in range(len(gridlist[grid][0][0]),max_grid_length): gridlist[grid][0][2].append(point)
+
+  # Catenate the grids into one large grid
+  for grid in range(1,len(gridlist)):
+   for block in range(len(gridlist[0])):
+    gridlist[0][block][1] += gridlist[grid][block][1]
+    for point in range(len(gridlist[grid][block][2])): gridlist[0][block][2][point] += gridlist[grid][block][2][point]
+  
   # Print the data out
-  output_table(datagrid[1:], settings, format)
+  output_table(gridlist[0], settings, format)
   return
 
 # FILTER_DATASET(): Filter data against the ranges supplied
@@ -165,6 +236,7 @@ def filter_dataset (datagrid, axes):
 # OUTPUT_TABLE(): Write the table of numbers produced out to a file
 
 def output_table (datagrid, settings, format):
+  print datagrid
   # Get the filename
   fname_out = os.path.expanduser(settings['OUTPUT'])
   if (fname_out == ""): fname_out = "pyxplot.dat"
